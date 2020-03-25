5f82c89052dbdb6a18541ff160ceec95652b67fc
Lilian Korir <likorir@davidson.edu>
2020-03-25 12:29:12 -0400

    CIS108secondassessment

--- src/avgScrabbleScoreFromFile.js
+++ src/avgScrabbleScoreFromFile.js
@@ -1,6 +1,6 @@
 let fs = require('fs');
 let avgScrabbleScore = require('./avgScrabbleScore');
-
+let scrabbleScore = require('./scrabbleScore');
 /**
  * Returns the average scrabble score of all the words the
  * given text file.
@@ -10,12 +10,24 @@ let avgScrabbleScore = require('./avgScrabbleScore');
  * Create some sample files yourself. We also supplied
  * sample-words.txt.
  */
+
 function avgScrabbleScoreFromFile(fileName) {
+  let array = fs.readFileSync(fileName).toString().split("\n");
+  let arraySum = 0;
+  for (let word of array) {
+    arraySum += scrabbleScore(word);
+  }
+  return (arraySum / (array.length - 1))
+
   // This is your job. :)
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for avgScrabbleScoreFromFile:');
+  console.log(avgScrabbleScoreFromFile('sample-words.txt'));
+  console.log(avgScrabbleScoreFromFile('names.txt'));
+  console.log(avgScrabbleScoreFromFile('aaa.txt')); //7
+  console.log(avgScrabbleScoreFromFile('days.txt'));
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
--- src/scrabbleScore.js
+++ src/scrabbleScore.js
@@ -1,8 +1,8 @@
 let SCRABBLE_TILE_SCORES = {
-  a: 1, b: 3, c: 3, d: 2, e: 1,  f: 4,
-  g: 2, h: 4, i: 1, j: 8, k: 5,  l: 1,
+  a: 1, b: 3, c: 3, d: 2, e: 1, f: 4,
+  g: 2, h: 4, i: 1, j: 8, k: 5, l: 1,
   m: 3, n: 1, o: 1, p: 3, q: 10, r: 1,
-  s: 1, t: 1, u: 1, v: 4, w: 4,  x: 8,
+  s: 1, t: 1, u: 1, v: 4, w: 4, x: 8,
   y: 4, z: 10,
 };
 
@@ -24,6 +24,13 @@ let SCRABBLE_TILE_SCORES = {
  */
 
 function scrabbleScore(word) {
+  word = word.toLowerCase();
+  let sum = 0;
+  for (let i = 0; i < word.length; ++i) {
+    sum += SCRABBLE_TILE_SCORES[word.charAt(i)] || 0;
+  }
+
+  return sum;
   // This is your job. :)
 }
 
@@ -32,6 +39,12 @@ if (require.main === module) {
 
   console.log(scrabbleScore('aaa') === 3);
   console.log(scrabbleScore('aaa') === scrabbleScore('AaA'));
+  console.log(scrabbleScore('abc')); //7
+  console.log(scrabbleScore('ggh')); //8
+  console.log(scrabbleScore('mother')); //11
+  console.log(scrabbleScore('ToDaAtE')); //8
+  console.log(scrabbleScore('CodING')); //10
+
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
